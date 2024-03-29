0b6d6ddfe2996a17c574523121f175153bfbbb1d
Kevyn Prada <koprada@javeriana.edu.co>
2024-03-29 11:09:42 -0500

    fix routing error

--- FrontEnd/src/app/modules/authentication/login/login.component.ts
+++ FrontEnd/src/app/modules/authentication/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  constructor(private router: Router) {}
+  
+  redirectToMain() {
+    this.router.navigateByUrl('/main');
+  }
 }
